c6de7728bc7e0a82c751ded138bb84a819bdee6c
zeekay <zeekayy@gmail.com>
2011-12-30 12:02:43 -0600

    Reorganize a bit

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -27,17 +27,7 @@
 (setq auto-save-file-name-transforms '((".*" "~/.emacs/autosaves/\\1" t)))
 
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- )
 
 ; derived from ELPA installation
 ; http://tromey.com/elpa/install.html
@@ -132,12 +122,3 @@
 (color-theme-molokai)
 
 
-;;; This was installed by package-install.el.
-;;; This provides support for the package system and
-;;; interfacing with ELPA, the package archive.
-;;; Move this code earlier if you want to reference
-;;; packages in your .emacs.
-(when
-    (load
-     (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
