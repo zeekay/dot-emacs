d9364f8052c13ea2bb83d5925466d8f503e3d410
Zach Kelling <zk@monoid.io>
2014-04-29 03:49:12 -0500

    Update to use latest ellipsis API.

--- ellipsis.sh
+++ ellipsis.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 #
 # zeekay/dot-emacs
+#
 # My emacs configuration.
 
 pkg.install() {
-    ellipsis.backup ~/.emacs
-    ellipsis.backup ~/.emacs.d
+    fs.backup ~/.emacs
+    fs.backup ~/.emacs.d
     ln -s $PKG_PATH ~/.emacs
     ln -s $PKG_PATH/.emacs.d ~/.emacs.d
 }
